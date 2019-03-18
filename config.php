6de3867c88fa4da46300c82f59a09193c05b8186
Unknown <robinbrongers2001@gmail.com>
2019-03-18 14:31:22 +0100

    Database Connectie

--- config.php
+++ config.php
@@ -5,3 +5,19 @@
  * Date: 18-3-2019
  * Time: 14:25
  */
+
+
+$dbHost = "localhost";
+$dbName = "degokkersphp";
+$dbUser = "root";
+$dbPassword = "";
+
+$dsn= "mysql:host=$dbHost;dbName=$dbName";
+$db = new PDO($dsn, $dbUser, $dbPassword);
+
+$db->setAttribute(PDO:: ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
+
+
+if($db){
+    echo "Connected to the $dbName database successfully!";
+}
