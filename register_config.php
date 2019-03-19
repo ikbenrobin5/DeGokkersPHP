ca6fd80eff7eaa891c884663fb727c1e6606cf4f
Unknown <robinbrongers2001@gmail.com>
2019-03-19 10:56:52 +0100

    Registreer config

--- register_config.php
+++ register_config.php
@@ -7,3 +7,22 @@
  */
 
 
+if ($_SERVER['REQUEST_METHOD'] != 'POST'){
+    header('Location: index.php');
+    exit;
+}
+
+require "config.php";
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$sql = "INSERT INTO profiles (email, password) VALUES ( :email, :password)";
+
+$prepare = $db->prepare($sql);
+$prepare->execute([
+    ':email' => $email,
+    ':password' => $password
+]);
+
+header('Location: login.php');
