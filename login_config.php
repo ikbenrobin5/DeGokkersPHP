9075d2746699d1e5b5e3afa73d97f128cb6ccc33
Jimmy <a-bogers72@ziggo.nl>
2019-03-19 14:24:38 +0100

    WTF

--- login_config.php
+++ login_config.php
@@ -12,3 +12,22 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST'){
     exit;
 }
 
+session_start();
+
+require 'config.php';
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    // Check if username is empty
+    if(empty(trim($_POST["email"]))){
+        $username_err = "Please enter username.";
+    }
+    else {
+        $username = trim($_POST["username"]);
+    }
