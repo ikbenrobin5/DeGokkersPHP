e7d089b0068c6b54b6ed185d18b44d3fc2d746a3
Unknown <robinbrongers2001@gmail.com>
2019-03-19 13:22:20 +0100

    Font family

---
+++ login_config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Robin
+ * Date: 19-3-2019
+ * Time: 13:16
+ */
+
+
+if ($_SERVER['REQUEST_METHOD'] != 'POST'){
+    header('Location: index.php');
+    exit;
+}
+
+require 'config.php';
+
